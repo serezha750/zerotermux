33227b9ca7a6e3d9632647be54583589924ef6b9
serezha750 <serezha750@users.noreply.github.com>
2026-09-17 02:57:30 +0000

    feat: 常用功能「发行版本」改为官方 proot-distro
    - 不再解压旧版 termux_linux_toolx.zip / termux-linux-install.py
    - 点击后：pkg install proot-distro、list 可用发行版并打印用法
    - 文档指向 https://github.com/termux/proot-distro

--- app/src/main/java/com/termux/zerocore/config/mainmenu/config/ReleaseLinuxVersionClickConfig.java
+++ app/src/main/java/com/termux/zerocore/config/mainmenu/config/ReleaseLinuxVersionClickConfig.java
@@ -6,15 +6,14 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.View;
 
-import com.example.xh_lib.utils.UUtils;
 import com.termux.R;
-import com.termux.app.TermuxActivity;
 import com.termux.zerocore.code.CodeString;
-import com.termux.zerocore.dialog.LoadingDialog;
-import com.termux.zerocore.url.FileUrl;
+import com.termux.zerocore.utils.SingletonCommunicationUtils;
 
-import java.io.File;
-
+/**
+ * 常用功能 → 发行版本：使用官方 proot-distro 管理发行版。
+ * https://github.com/termux/proot-distro
+ */
 public class ReleaseLinuxVersionClickConfig extends BaseMenuClickConfig {
     @Override
     public int getType() {
@@ -33,21 +32,9 @@ public class ReleaseLinuxVersionClickConfig extends BaseMenuClickConfig {
 
     @Override
     public void onClick(View view, Context context) {
-        LoadingDialog loadingDialog = new LoadingDialog(context);
-        loadingDialog.show();
-        loadingDialog.setCancelable(false);
-        UUtils.runOnThread(new Runnable() {
-            @Override
-            public void run() {
-                UUtils.writerFile("linux/termux_linux_toolx.zip", new File(FileUrl.INSTANCE.getMainHomeUrl(), "/termux_linux_toolx.zip"));
-                UUtils.runOnThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        loadingDialog.dismiss();
-                        com.termux.zerocore.utils.SingletonCommunicationUtils.getInstance().getmSingletonCommunicationListener().sendTextToTerminal(CodeString.INSTANCE.getRunLinuxSh());
-                    }
-                });
-            }
-        });
+        // 安装 proot-distro（若已安装则跳过更新耗时由 pkg 处理），列出可用发行版并提示用法
+        SingletonCommunicationUtils.getInstance()
+            .getmSingletonCommunicationListener()
+            .sendTextToTerminal(CodeString.INSTANCE.getRunLinuxSh());
     }
 }
